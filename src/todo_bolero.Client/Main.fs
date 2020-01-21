dfff6e1a799cbc16886da553af1fb0062433e523
Taylor Fraley <tfraley@clearent.com>
2020-01-21 14:58:23 -0500

    removing ionide from gitignore; fixes indents

--- src/todo_bolero.Client/Main.fs
+++ src/todo_bolero.Client/Main.fs
@@ -18,55 +18,55 @@ type Entry =
   }
 
 type Model =
-    {
-        Page: Page
-        Name: string
-        Entries: Entry list
-    }
+  {
+      Page: Page
+      Name: string
+      Entries: Entry list
+  }
 
 let initModel =
-    {
-        Page = Home
-        Name = "test list"
-        Entries = [
-          { 
-            Id = Guid.NewGuid()
-            Value = "first"
-          }
-          {
-            Id = Guid.NewGuid()
-            Value = "second"
-          }
-          {
-            Id = Guid.NewGuid()
-            Value = "third"
-          }
-        ]
-    }
+  {
+      Page = Home
+      Name = "test list"
+      Entries = [
+        { 
+          Id = Guid.NewGuid()
+          Value = "first"
+        }
+        {
+          Id = Guid.NewGuid()
+          Value = "second"
+        }
+        {
+          Id = Guid.NewGuid()
+          Value = "third"
+        }
+      ]
+  }
 
 /// The Elmish application's update messages.
 type Message =
-    | SetPage of Page
-    | SetName of string
-    | AddEntry of string
-    | RemoveEntry of Guid
+  | SetPage of Page
+  | SetName of string
+  | AddEntry of string
+  | RemoveEntry of Guid
 
 let update message model =
-   match message with
-    | SetPage page ->
-        { model with Page = page }
-    | SetName str ->
-        { model with Name = str }
-    | AddEntry str ->
-        { model with Entries = 
-                     model.Entries @ 
-                      [{
-                        Id = Guid.NewGuid()
-                        Value = str
-                      }] }
-    | RemoveEntry id -> 
-        { model with Entries =
-                      model.Entries |> List.filter (fun x -> x.Id <> id) }
+  match message with
+  | SetPage page ->
+      { model with Page = page }
+  | SetName str ->
+      { model with Name = str }
+  | AddEntry str ->
+      { model with Entries = 
+                   model.Entries @ 
+                    [{
+                      Id = Guid.NewGuid()
+                      Value = str
+                    }] }
+  | RemoveEntry id -> 
+      { model with Entries =
+                    model.Entries |> List.filter (fun x -> x.Id <> id) }
 
 /// Connects the routing system to the Elmish application.
 let router = Router.infer SetPage (fun model -> model.Page)
@@ -74,23 +74,23 @@ let router = Router.infer SetPage (fun model -> model.Page)
 type Main = Template<"wwwroot/main.html">
 
 let showEntry dispatch (entry: Entry) =
-    Main.Entry()
-      .Id(entry.Id)
-      .Value(entry.Value)
-      .RemoveEntry(fun _ -> dispatch (RemoveEntry entry.Id))
-      .Elt()
+  Main.Entry()
+    .Id(entry.Id)
+    .Value(entry.Value)
+    .RemoveEntry(fun _ -> dispatch (RemoveEntry entry.Id))
+    .Elt()
 
 let view (model:Model) dispatch =
-    Main()
-      .ListName(model.Name)
-      .Entries(forEach model.Entries <| fun e -> showEntry dispatch e)
-      .AddEntry("", fun str -> dispatch (AddEntry str))
-      .Elt()
+  Main()
+    .ListName(model.Name)
+    .Entries(forEach model.Entries <| fun e -> showEntry dispatch e)
+    .AddEntry("", fun str -> dispatch (AddEntry str))
+    .Elt()
 
 type MyApp() =
-    inherit ProgramComponent<Model, Message>()
+  inherit ProgramComponent<Model, Message>()
 
-    override this.Program =
-        Program.mkSimple (fun _ -> initModel) update view
-        |> Program.withRouter router
-        |> Program.withHotReload
+  override this.Program =
+    Program.mkSimple (fun _ -> initModel) update view
+    |> Program.withRouter router
+    |> Program.withHotReload
